cbc5ad02d2ecf55f51a53e97329390d619505593
103620582 <103620582@student.swin.edu.au>
2021-05-10 11:35:11 +1000

    Task 2 Complete

--- SQL_Build_Query.sql
+++ SQL_Build_Query.sql
@@ -1,8 +1,5 @@
 /*
-Joy Ambrose -- 103620582
-*/
-
-/* 
+Joy Ambrose -- 10362058
 
 TOUR (TourName, Description)
 PK - TourName
@@ -14,7 +11,54 @@ FK - (TourName) References TOUR
 CLIENT (ClientID, Surname, GivenName, Gender)
 PK - (ClientID)
 
-BOOKING (DateBooked, Payment, EventYear, EventMonth, EventDay, ClientID, )
-PK - (DateBooked)
-FK - (EventYear, EventMonth, EventDay) References Event
+BOOKING (ClientID, TourName, EventMonth, EventDay, Payment, DateBooked)
+PK - (ClientID, Tourname, EventMonth, EventDay, EventYear)
 FK - (ClientID) References CLIENT
+FK - (TourName) References TOUR
+FK - (EventYear, EventMonth, EventDay) References Event
+
+*/
+
+
+
+CREATE TABLE TOUR (
+    TourName    NVARCHAR(100)
+,   Description NVARCHAR(500)
+,   PRIMARY KEY (TourName)
+);
+
+CREATE TABLE CLIENT (
+    ClientID    INT
+,   Surname     NVARCHAR(100) NOT NULL
+,   GivenName   NVARCHAR(100) NOT NULL
+,   Gender      NVARCHAR(1) 
+,   PRIMARY KEY (ClientID)
+);
+
+CREATE TABLE EVENT (
+    TourName    NVARCHAR(100)
+,   EventMonth  NVARCHAR(3)
+,   EventDay    INT
+,   EventYear   INT
+,   EventFee    MONEY NOT NULL
+,   PRIMARY KEY (EventYear, EventMonth, EventDay)
+,   FOREIGN KEY (TourName) REFERENCES TOUR (TourName)
+);
+
+CREATE TABLE BOOKING (
+    ClientID    INT
+,   TourName    NVARCHAR(100)
+,   EventMonth  NVARCHAR(3)
+,   EventDay    INT 
+,   EventYear   INT
+,   Payment     INT
+,   DateBooked  DATE NOT NULL
+,   PRIMARY KEY (ClientID, TourName, EventMonth, EventDay, EventYear)
+,   FOREIGN KEY (ClientID) REFERENCES CLIENT (ClientID)
+,   FOREIGN KEY (TourName) REFERENCES TOUR (TourName)
+,   FOREIGN KEY (EventYear, EventMonth, EventDay) REFERENCES EVENT (EventYear, EventMonth, EventDay)
+);
+
+
+SELECT *
+FROM INFORMATION_SCHEMA.TABLES
